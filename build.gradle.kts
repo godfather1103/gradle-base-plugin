37240fd9faa78a0356d7d8f90240c9e5d63a1b7b
Jack Chu <chuchuanbao@gmail.com>
2021-05-12 03:38:23 +0800

    feat(init): 增加说明信息

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,11 @@ plugins {
 
 gradlePlugin {
     plugins {
-        create("gradle-base-plugin") {
-            id = "com.github.godfather1103"
-            implementationClass = "gradle.base.plugin"
+        create("gradleBasePlugin") {
+            id = "com.github.godfather1103.gradle.base.plugin"
+            implementationClass = "Gradle_base_pluginPlugin"
+            displayName = "Gradle Base Plugin"
+            description = "Gradle Base Plugin"
         }
     }
 }
@@ -19,10 +21,11 @@ version = "1.0"
 pluginBundle {
     website = "https://github.com/godfather1103"
     vcsUrl = "https://github.com/godfather1103/gradle-base-plugin"
-    description = "gradle base plugin"
+    description = "Gradle Base Plugin"
     (plugins){
-        "gradle-base-plugin"{
+        "gradleBasePlugin" {
             displayName = "Gradle Base Plugin"
+            description = "Gradle Base Plugin"
             tags = listOf("clean", "compile", "build")
             version = "1.0"
         }
@@ -31,6 +34,7 @@ pluginBundle {
         groupId = "com.github.godfather1103"
         artifactId = "gradle-base-plugin"
         version = "1.0"
+        description = "Gradle Base Plugin"
     }
 }
 
