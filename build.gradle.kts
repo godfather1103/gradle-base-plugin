35533036c1f4e1c9d4569de2626ec98bced287da
Jack Chu <chuchuanbao@gmail.com>
2021-05-12 02:54:48 +0800

    feat(init): 增加版本信息

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ gradlePlugin {
     }
 }
 
+group = "com.github.godfather1103"
+version = "1.0"
+
 pluginBundle {
     website = "https://github.com/godfather1103"
     vcsUrl = "https://github.com/godfather1103/gradle-base-plugin"
