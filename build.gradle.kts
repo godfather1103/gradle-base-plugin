44f83143e8f2383261ff1d458b25d40bb6697938
Jack Chu <chuchuanbao@gmail.com>
2021-05-12 02:31:39 +0800

    feat(init): 项目初始化

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "0.14.0"
+    `java-gradle-plugin`
+}
+
+gradlePlugin {
+    plugins {
+        create("gradle-base-plugin") {
+            id = "com.github.godfather1103"
+            implementationClass = "gradle.base.plugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/godfather1103"
+    vcsUrl = "https://github.com/godfather1103/gradle-base-plugin"
+    description = "gradle base plugin"
+    (plugins){
+        "gradle-base-plugin"{
+            displayName = "Gradle Base Plugin"
+            tags = listOf("clean", "compile", "build")
+            version = "1.0"
+        }
+    }
+    mavenCoordinates {
+        groupId = "com.github.godfather1103"
+        artifactId = "gradle-base-plugin"
+        version = "1.0"
+    }
+}
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "gradle-base-plugin"
+
