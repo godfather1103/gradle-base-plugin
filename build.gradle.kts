0b7a8a5410eeac67a3096892577077aaeac2ea6f
Jack Chu <chuchuanbao@gmail.com>
2021-05-15 02:52:22 +0800

    build(gradle): 优化maven仓库

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,8 +56,8 @@ publishing {
     }
     repositories {
         maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
             url = if (version.toString().toUpperCase().contains("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
             credentials {
                 username = "${property("ossrhUsername")}"
