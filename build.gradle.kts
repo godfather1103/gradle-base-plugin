6260e18a7aaf09b8a78c0c638fc42b3253d357dd
Jack Chu <chuchuanbao@gmail.com>
2021-05-15 01:45:56 +0800

    build(gradle): 补充maven上传需要的属性

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,78 @@ plugins {
     id("com.gradle.plugin-publish") version "0.14.0"
     `java-gradle-plugin`
     `maven-publish`
+    signing
 }
 
 dependencies {
     implementation(gradleApi())
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "${property("plugin.artifactId")}"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("${property("plugin.artifactId")}")
+                description.set("${property("plugin.description")}")
+                url.set("https://github.com/godfather1103/gradle-base-plugin")
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("https://mit-license.org/")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("godfather1103")
+                        name.set("Jack Chu")
+                        email.set("chuchuanbao@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:godfather1103/gradle-base-plugin.git")
+                    developerConnection.set("scm:git:git@github.com:godfather1103/gradle-base-plugin.git")
+                    url.set("https://github.com/godfather1103/gradle-base-plugin")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().toUpperCase().contains("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = "${property("ossrhUsername")}"
+                password = "${property("ossrhPassword")}"
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
 }
 
 gradlePlugin {
@@ -32,13 +100,13 @@ pluginBundle {
             displayName = "${property("plugin.displayName")}"
             description = "${property("plugin.description")}"
             tags = listOf("clean", "compile", "build")
-            version = "1.0"
+            version = "${property("plugin.version")}"
         }
     }
     mavenCoordinates {
-        groupId = "com.github.godfather1103"
-        artifactId = "gradle-base-plugin"
-        version = "1.0"
+        groupId = "${property("plugin.groupId")}"
+        artifactId = "${property("plugin.artifactId")}"
+        version = "${property("plugin.version")}"
         description = "${property("plugin.description")}"
     }
 }
