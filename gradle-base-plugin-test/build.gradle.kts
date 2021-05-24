c6c2dd9eaba358b974c5f50b2fd77d7f515f9901
Jack Chu <chuchuanbao@gmail.com>
2021-05-25 01:26:39 +0800

    test(plugin): 增加filter参数的测试用例

--- gradle-base-plugin-test/build.gradle.kts
+++ gradle-base-plugin-test/build.gradle.kts
@@ -9,7 +9,9 @@ buildscript {
 apply(plugin = "${property("plugin.groupId")}.${property("plugin.artifactId")}")
 
 configure<com.github.godfather1103.GradleBasePlugin.BasePluginExtension> {
-    param.put("one","1")
-//    param.put("two","2")
-    files.set("filters/a.properties")
+    param.put("one", "1")
+    if (System.getProperty("env") == "ccb") {
+        param.put("two", "2")
+    }
+    files.set(project.projectDir.absolutePath + "/filters/a.properties")
 }
