468e40acd49073b927329ee9f11a8eae3fde0cc8
Jack Chu <chuchuanbao@gmail.com>
2021-05-25 01:02:37 +0800

    feat(plugin): 增加filter参数的功能

--- gradle-base-plugin-test/build.gradle.kts
+++ gradle-base-plugin-test/build.gradle.kts
@@ -7,3 +7,9 @@ buildscript {
     }
 }
 apply(plugin = "${property("plugin.groupId")}.${property("plugin.artifactId")}")
+
+configure<com.github.godfather1103.GradleBasePlugin.BasePluginExtension> {
+    param.put("one","1")
+//    param.put("two","2")
+    files.set("filters/a.properties")
+}
